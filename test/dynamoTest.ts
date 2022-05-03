44a12757578cf5980e9f08a390f45b5c3e924f7f
vsosahdz <vsosa@tec.mx>
2022-05-03 16:32:42 -0500

    final

--- test/dynamoTest.ts
+++ test/dynamoTest.ts
@@ -3,12 +3,12 @@
 const dynamodb = require("dynamodb");
 const Joi = require('joi');
 
-//Configuración del SDK en nodejs para poder trabajar con la nube
+//Configuración del SDK en nodejs para poder trabajar con la nube llenar credenciales
 dynamodb.AWS.config.update({
-    accessKeyId:"ASIAWFVAQPIG6C2R2AYC",
-    secretAccessKey:"XLTM3ggISLDNRD8+sv5AqBXMHv38+qMYI1MNqZaT",
+    accessKeyId:"",
+    secretAccessKey:"",
     //unicamente necesario con el learner lab
-    sessionToken:"FwoGZXIvYXdzEDcaDIbiwwFchBzynHN9WCK2AQskD9GETCyB4KumODUvfXGwn7dn6d/boPoHxrN41wNjiaovYd7AViLNetQ4RtFVUWpm777Qdw4Y8klZ4b/WH81wQsDEmjTXtLWQrnMm+5I49TsNjG7kjSMtdKaY7KVIaD+YnZ6YEbUzMhZhyoWWOulJCVrQjidDJID86K938LAQu6GpnFCYh6ABg99fo+mAspKFKW8OI1qWIN3r8LagWWibJBmsc1eZNAbQQ6ND1P/ac3EsQ6KUKIaIrJMGMi28fcZCPF3n+Pgyo9rMld4iZgl6S8bnyiAc2zeYuHg4jC5lApGqOSl+Rz/RCsY=",
+    sessionToken:"",
     region:"us-east-1"
 });
 
